86729f762ba3636b3f39152eaba082eab618e27c
Anh DH <anhdhbn@gmail.com>
2020-02-22 20:32:23 -0800

    init

--- req.py
+++ req.py
@@ -4,7 +4,7 @@ import base64
 with open("test.png", "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read())
 
-req = requests.post("http://127.0.0.1:5000/api/ocr", json={
+req = requests.post("https://tesseract-server-anhdh.herokuapp.com/api/ocr", json={
     "imagestr": encoded_string.decode() 
 })
 
